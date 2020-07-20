1212331dd3d2e08bf56c590a836b77b3a1e27abf
fang <68498585+Patcharapornn@users.noreply.github.com>
2020-07-20 16:51:45 +0700

    Add files via upload

---
+++ firstproject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "1st Term of 2020 \t\t \\45133114   Wanthanee Prachuabsupakij" << endl;
+	cout << "---------------------------------------------------------------------------------" << endl;
+	cout << "| Subject ID |    Subject Name              | Score (100) | Grad | " << endl;
+	cout << "---------------------------------------------------------------------------------" << endl;
+	cout << "| 4570034    |  Introduction to computer    |    85       | \"A\" | " << endl;
+	cout << "| 4580247    |  English                     |    74       | \"B\" | " << endl;
+	cout << "| 4530001    |  Design Thinking             |    92       | \"A\" | " << endl;
+	cout << "---------------------------------------------------------------------------------" << endl;
+	cout << "| Total      |  Subject = 3                 | Average = 3.87 | " << endl;
+	cout << "=====================================*=== " << endl;
+}
